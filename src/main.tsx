d415b0558d61e21fdd6749a84c3957dc5b222812
Danut Codrescu <danut.codrescu@collibra.com>
2025-06-30 15:42:25 +0200

    support tailwind via postcss as oxide is not supported in stackblitz

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./style.css"; // Ensure this path is correct based on your project structure
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
