c96c9ada4689c2dff261f5b202afd2772d803b79
Danut Codrescu <danut.codrescu@collibra.com>
2025-06-30 15:49:57 +0200

    support tailwind via cdn for stackblitz

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./style.css"; // Ensure this path is correct based on your project structure
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
