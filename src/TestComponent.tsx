b8e05877afb24b3918e8e139c3c90292a584727e
Danut Codrescu <danut.codrescu@collibra.com>
2025-07-01 14:26:29 +0200

    remove the tw div from the starter code

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,12 +1,9 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 export function TestComponent() {
   return (
-    <>
-      <div className="bg-blue-500 text-white p-4 rounded-lg">
-        Hello, Tailwind CSS!
-      </div>
+    <Stack>
       <Typography>Placeholder text</Typography>
-    </>
+    </Stack>
   );
 }
